0e58a74b1bd179ae22d302a6564cfb5f7e9ae739
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2014-01-23 09:54:48 +0200

    Small changes to the way the regression tests are launched
    Running the regression tests required a server to be running, because
    pg_regress launches the test scripts using psql against the running server.
    But that server wasn't actually used for anything, the test driver initdb's
    a fresh installation and does all the interesting stuff with that.
    This patch removes the requirement for an existing server.
    
    pg_regress normally launches the test scripts using psql, but launching
    psql requires a server to be running, so use a custom launcher script that
    runs the .sql files using a shell instead.
    
    Also, use ./pg_rewind instead of just pg_rewind in the test script. The
    current directory isn't usually in $PATH, and presumably we want to test the
    version that's compiled.

--- sql/pg_rewind_remote.sql
+++ sql/pg_rewind_remote.sql
@@ -1,1 +1,6 @@
-\! sh sql/pg_rewind.sh --remote
+# This file has the .sql extension, but it is actually launched as a shell
+# script. This contortion is necessary because pg_regress normally uses
+# psql to run the inptu scripts, and requires them to have the .sql
+# extension, but we use a custom launcher script that runs the scripts using
+# a shell instead.
+sql/pg_rewind.sh --remote
