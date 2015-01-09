a186106ae3dc3f11a4dc9ae5e233c4ddb265b091
Michael Paquier <michael@otacoo.com>
2015-01-09 10:15:34 +0900

    Remove copyright notice of NTT originally here for XLOG parsing code
    This code has been taken from pg_lesslog, and it has been removed since
    upstream offers the possibility to track block information more easily
    in 9.5. So this copyright notice is not needed now.

--- parsexlog.c
+++ parsexlog.c
@@ -5,7 +5,6 @@
  *
  * Portions Copyright (c) 2013-2015 VMware, Inc. All Rights Reserved.
  * Portions Copyright (c) 1996-2015, PostgreSQL Global Development Group
- * Portions Copyright (c) 1996-2008, Nippon Telegraph and Telephone Corporation
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  *-------------------------------------------------------------------------
